3055d6940de038c5b7e37360e29a10aeceab609c
Date Calculator Builder <builder@datecalculator.app>
2026-07-19 09:15:13 +0000

    修复仓库配置，使用标准 Google 和 Maven Central 仓库

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,25 +1,5 @@
 // 顶层构建文件 - 日期时间计算器
-buildscript {
-    repositories {
-        maven { url = uri("file:///data/user/work/local-maven-repo") }
-        maven { url = uri("https://maven.aliyun.com/repository/google") }
-        maven { url = uri("https://maven.aliyun.com/repository/central") }
-        maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        classpath("com.android.tools.build:gradle:8.2.0")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
-    }
-}
-
-allprojects {
-    repositories {
-        maven { url = uri("file:///data/user/work/local-maven-repo") }
-        maven { url = uri("https://maven.aliyun.com/repository/google") }
-        maven { url = uri("https://maven.aliyun.com/repository/central") }
-        google()
-        mavenCentral()
-    }
+plugins {
+    id("com.android.application") version "8.2.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
 }
